eca2fec3c058ba37f5c329a287d55fe1fb3933da
Roy Versteeg <mail@royversteeg.nl>
2022-12-06 09:31:59 +0100

    Implement day 6

--- Util/PuzzleSolutionWithTextInput.cs
+++ Util/PuzzleSolutionWithTextInput.cs
@@ -2,6 +2,7 @@
 {
     public abstract class PuzzleSolutionWithTextInput : PuzzleSolutionWithParsedInput<string>
     {
+        public PuzzleSolutionWithTextInput() { }
         protected PuzzleSolutionWithTextInput(int day, int year) : base(day, year) { }
 
         protected override string Parse() => ReadAllInputText();
---
+++ Y2022/Day06.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+using AdventOfCode.Util;
+
+namespace AdventOfCode.Y2022;
+
+public class Day06 : PuzzleSolutionWithTextInput
+{
+    public override object SolvePart1(string input)
+        => FindMarker(input, 4);
+
+    public override object SolvePart2(string input)
+        => FindMarker(input, 14);
+
+    private static int FindMarker(string input, int markerLength)
+        => Enumerable.Range(0, input.Length - markerLength)
+            .First(x => input[x..(x + markerLength)].Distinct().Count() == markerLength) + markerLength;
+}
