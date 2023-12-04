eba6e9aaef8d5f58dee741255ecd13e862600b09
Roy Versteeg <mail@royversteeg.nl>
2023-12-04 17:54:27 +0100

    Update

--- Y2023/Day03.cs
+++ Y2023/Day03.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using System.Reflection;
 using AdventOfCode.Util;
+using AdventOfCode.Y2021;
 using Xunit;
 
 namespace AdventOfCode.Y2023;
@@ -63,6 +64,8 @@ public class Day03 : PuzzleWithCharGrid
 
     public override object SolvePart2(char[][] input)
     {
+        var lookup = GetValues(input).SelectMany(x => x.Edges.Select(y => (y, x.Value))).ToLookup(x => x.y, x => x.Value);
+        
         var total = 0;
         for (int y = 0; y < input.Length; y++)
         {
@@ -70,15 +73,51 @@ public class Day03 : PuzzleWithCharGrid
             {
                 if(input[y][x] != '*')
                     continue;
-
-                var adjacentParts = GetAdjacentParts(x, y, input).ToList();
-
-                if (adjacentParts.Count == 2)
-                    total += adjacentParts[0] * adjacentParts[1];
+                var point = new Point(x, y);
+                if (lookup.Contains(point) && lookup[point].Count() == 2)
+                {
+                    total += lookup[point].First() * lookup[point].Last();
+                }
             }
         }
 
         return total;
+    }
+
+    private IEnumerable<(int Value, List<Point> Edges)> GetValues(char[][] input)
+    {
+        for (int y = 0; y < input.Length; y++)
+        {
+            int value = 0;
+            List<Point> edges = new();
+            for (int x = 0; x <= input[0].Length; x++)
+            {
+                if (x == input[0].Length || !char.IsDigit(input[y][x]))
+                {
+                    if (value > 0)
+                    {
+                        edges.Add(new Point(x + 1, y -1));
+                        edges.Add(new Point(x + 1, y));
+                        edges.Add(new Point(x + 1, y + 1));
+                        yield return (value, edges);
+                        edges = new List<Point>();
+                        value = 0;
+                    }
+                    continue;
+                }
+
+                if (value == 0)
+                {
+                    edges.Add(new Point(x - 1, y -1));
+                    edges.Add(new Point(x - 1, y));
+                    edges.Add(new Point(x - 1, y + 1));
+                }
+
+                edges.Add(new Point(x, y - 1));
+                edges.Add(new Point(x, y + 1));
+                value = value * 10 + (input[y][x] - '0');
+            }
+        }
     }
 
     private IEnumerable<int> GetAdjacentParts(int x, int y, char[][] input)
