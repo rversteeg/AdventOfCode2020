2af04a19a60caea036ff5224c3494c1b4f92d136
Roy Versteeg <mail@royversteeg.nl>
2024-12-06 09:06:30 +0100

    Minor refactor

--- Y2024/Day06.cs
+++ Y2024/Day06.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using AdventOfCode.Util;
 using Pos = (int X, int Y);
 using Dir = (int X, int Y);
@@ -9,8 +8,8 @@ namespace AdventOfCode.Y2024;
 
 public class Day06 : PuzzleWithCharGrid
 {
-    private Dir[] directions = [(0, -1), (1, 0), (0, 1), (-1, 0)];
-    private char[] directionsChars = ['^', '>', 'v', '<'];
+    private static readonly Dir[] Directions = [(0, -1), (1, 0), (0, 1), (-1, 0)];
+    private static readonly char[] DirectionsChars = ['^', '>', 'v', '<'];
     
     public override object SolvePart1(char[][] input)
     {
@@ -26,7 +25,7 @@ public class Day06 : PuzzleWithCharGrid
             if (curChar is not '.' and not '#')
             {
                 curPos = gridPos;
-                dirIndex = Array.IndexOf(directionsChars, curChar);
+                dirIndex = Array.IndexOf(DirectionsChars, curChar);
             }
         }
         do
@@ -38,9 +37,9 @@ public class Day06 : PuzzleWithCharGrid
         return visited.Keys.Count;
     }
 
-    private (Pos pos, int dirIndex) Move(Dictionary<Pos, char> grid, Pos pos, int dirIndex)
+    private static (Pos pos, int dirIndex) Move(Dictionary<Pos, char> grid, Pos pos, int dirIndex)
     {
-        var dir = directions[dirIndex];
+        var dir = Directions[dirIndex];
         var nextPos = (pos.X + dir.X, pos.Y + dir.Y);
         if(grid.TryGetValue(nextPos, out var nextChar) && nextChar == '#')
             return Move(grid, pos, (dirIndex + 1) % 4);
@@ -61,7 +60,7 @@ public class Day06 : PuzzleWithCharGrid
             if (curChar is not '.' and not '#')
             {
                 curPos = gridPos;
-                dirIndex = Array.IndexOf(directionsChars, curChar);
+                dirIndex = Array.IndexOf(DirectionsChars, curChar);
             }
         }
         var startPos = curPos;
@@ -87,7 +86,7 @@ public class Day06 : PuzzleWithCharGrid
         return result;
     }
 
-    private bool IsCyclical(Dictionary<Pos,char> grid, Pos startPos, int startDirIndex)
+    private static bool IsCyclical(Dictionary<Pos,char> grid, Pos startPos, int startDirIndex)
     {
         var visited = new Dictionary<Pos, HashSet<int>>();
         var curPos = startPos;
@@ -98,7 +97,7 @@ public class Day06 : PuzzleWithCharGrid
             if(visited.TryGetValue(curPos, out var visitedDirs) && visitedDirs.Contains(dirIndex))
                 return true;
             
-            visited.TryAdd(curPos, new HashSet<int>());
+            visited.TryAdd(curPos, []);
             visited[curPos].Add(dirIndex);
             (curPos, dirIndex) = Move(grid, curPos, dirIndex);
         } while (grid.ContainsKey(curPos));
