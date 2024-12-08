8943e6ec6b48c6a6d9629ea8130b3eb6fa1fc8b7
Roy Versteeg <mail@royversteeg.nl>
2024-12-08 21:10:48 +0100

    Day 8 part 2

--- Y2024/Day08.cs
+++ Y2024/Day08.cs
@@ -42,6 +42,37 @@ public class Day08 : PuzzleWithCharGrid
 
     public override object SolvePart2(char[][] input)
     {
-        return 0;
+        var antennas = EnumerableExtensions.RangeGrid(input[0].Length, input.Length)
+            .Where(pos => input[pos.Y][pos.X] != '.')
+            .Select(pos => (Pos: pos, Type: input[pos.Y][pos.X]))
+            .ToList();
+
+        var types = antennas.ToLookup(x => x.Type, x => x.Pos);
+        var antinodes = new HashSet<(int X, int Y)>();
+        
+        foreach(var group in types)
+        {
+            foreach (var antenna in group)
+            {
+                foreach (var other in group)
+                {
+                    if(other == antenna)
+                        continue;
+                    
+                    antinodes.Add(antenna);
+                    
+                    var diff = (X: other.X - antenna.X, Y: other.Y - antenna.Y);
+                    var antinode = (X: other.X + diff.X, Y: other.Y + diff.Y);
+                    while (antinode.X >= 0 && antinode.X < input[0].Length && antinode.Y >= 0 &&
+                        antinode.Y < input.Length)
+                    {
+                        antinodes.Add(antinode);
+                        antinode = (X: antinode.X + diff.X, Y: antinode.Y + diff.Y);
+                    }
+                }
+            }
+        }
+        
+        return antinodes.Count;
     }
 }
