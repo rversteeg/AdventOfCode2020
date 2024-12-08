ad3a727ea16180e7c1414f59e3a8ca5d563b7aba
Roy Versteeg <mail@royversteeg.nl>
2024-12-08 20:55:32 +0100

    Day 7 and 8 part 1

---
+++ Y2024/Day07.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Linq;
+using AdventOfCode.Util;
+using Input = (long Result, long[] Items);
+
+namespace AdventOfCode.Y2024;
+
+public class Day07 : PuzzleSolutionWithParsedInput<(long Result, long[] Items)[]>
+{
+    public override object SolvePart1(Input[] input)
+        => input.Where(x => IsPossible(x.Result, x.Items[0], x.Items.AsSpan()[1..])).Sum(x => x.Result);
+    
+    private static bool IsPossible(long result, long current, ReadOnlySpan<long> numbers)
+    {
+        if (current > result)
+            return false;
+        
+        if(numbers.Length == 0)
+            return current == result;
+
+        var slice = numbers[1..];
+        
+        return IsPossible(result, current + numbers[0], slice) ||
+               IsPossible(result, current * numbers[0], slice);
+    }
+    
+    private static bool IsPossible2(long result, long current, ReadOnlySpan<long> numbers)
+    {
+        if (current > result)
+            return false;
+        
+        if(numbers.Length == 0)
+            return current == result;
+
+        var slice = numbers[1..];
+        
+        return IsPossible2(result, current + numbers[0], slice) ||
+               IsPossible2(result, current * numbers[0], slice) ||
+               IsPossible2(result, Int64.Parse($"{current}{numbers[0]}"), slice);
+    }
+
+    public override object SolvePart2(Input[] input) 
+        => input.Where(x => IsPossible2(x.Result, x.Items[0], x.Items.AsSpan()[1..])).Sum(x => x.Result);
+
+    protected override Input[] Parse()
+    {
+        return ReadAllInputLines().Select(ParseLine).ToArray();
+    }
+
+    private Input ParseLine(string line)
+    {
+        var parts = line.Split(':');
+        var numbers = parts[1].Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(Int64.Parse).ToArray();
+        var result = Int64.Parse(parts[0]);
+        return (result, numbers);
+    }
+}
---
+++ Y2024/Day08.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AdventOfCode.Util;
+
+namespace AdventOfCode.Y2024;
+
+public class Day08 : PuzzleWithCharGrid
+{
+    public override object SolvePart1(char[][] input)
+    {
+        var antennas = EnumerableExtensions.RangeGrid(input[0].Length, input.Length)
+            .Where(pos => input[pos.Y][pos.X] != '.')
+            .Select(pos => (Pos: pos, Type: input[pos.Y][pos.X]))
+            .ToList();
+
+        var types = antennas.ToLookup(x => x.Type, x => x.Pos);
+        var antinodes = new HashSet<(int X, int Y)>();
+        
+        foreach(var group in types)
+        {
+            foreach (var antenna in group)
+            {
+                foreach (var other in group)
+                {
+                    if(other == antenna)
+                        continue;
+                    
+                    var diff = (X: other.X - antenna.X, Y: other.Y - antenna.Y);
+                    var antinode = (X: other.X + diff.X, Y: other.Y + diff.Y);
+                    if (antinode.X >= 0 && antinode.X < input[0].Length && antinode.Y >= 0 &&
+                        antinode.Y < input.Length)
+                    {
+                        antinodes.Add(antinode);
+                    }
+                }
+            }
+        }
+        
+        return antinodes.Count;
+    }
+
+    public override object SolvePart2(char[][] input)
+    {
+        return 0;
+    }
+}
